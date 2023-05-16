5ef36f3a2377a2cc997b1748ca9de3cc3f9941b6
jari.valentine <jari.valentine@student.howest.be>
2023-05-16 17:08:17 +0200

    web api setup

---
+++ Howest.Magic.WebAPI/Controllers/CardsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Howest.MagicCards.DAL.Repositories;
+
+namespace Howest.MagicCards.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        private readonly CardRepository _cardRepo;
+
+        public CardsController(CardRepository cardRepo)
+        {
+            _cardRepo = cardRepo;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
