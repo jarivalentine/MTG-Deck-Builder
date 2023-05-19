2822d015bba8c0d1e4a926a9be66eb4b72e7d51f
jari.valentine <jari.valentine@student.howest.be>
2023-05-19 15:39:09 +0200

    related artists

---
+++ Howest.MagicCards.GraphQL/GraphQL/Types/ArtistType.cs
@@ -0,0 +1,16 @@
+﻿using GraphQL.Types;
+using Howest.MagicCards.DAL.Models;
+
+namespace Howest.MagicCards.GraphQL.GraphQL.Types;
+
+public class ArtistType : ObjectGraphType<Artist>
+{
+    public ArtistType()
+    {
+        Name = "Artist";
+
+        Field(a => a.Id, type: typeof(LongGraphType));
+        Field(a => a.FullName, type: typeof(StringGraphType));
+    }
+
+}
--- Howest.MagicCards.GraphQL/GraphQL/Types/CardType.cs
+++ Howest.MagicCards.GraphQL/GraphQL/Types/CardType.cs
@@ -2,6 +2,7 @@
 using GraphQL.Types;
 using Howest.MagicCards.DAL.Models;
 using Howest.MagicCards.DAL.Repositories;
+using Howest.MagicCards.GraphQL.GraphQL.Types;
 using Howest.MagicCards.Shared.DTO;
 
 namespace Howest.MagicCards.GraphQL.GraphQLTypes;
@@ -12,9 +13,12 @@ public class CardType : ObjectGraphType<Card>
     {
         Name = "Card";
 
-        Field(c => c.Name, type: typeof(StringGraphType))
-            .Description("Name of the card");
-        Field(c => c.Text, type: typeof(StringGraphType))
-            .Description("Text of the card");
+        Field(c => c.Name, type: typeof(StringGraphType));
+        Field(c => c.Type, type: typeof(StringGraphType));
+        Field(c => c.Text, type: typeof(StringGraphType));
+        Field(c => c.ManaCost, type: typeof(StringGraphType));
+        Field(c => c.Power, type: typeof(StringGraphType));
+        Field(c => c.Toughness, type: typeof(StringGraphType));
+        Field(c => c.Artist, type: typeof(ArtistType));
     }
 }
